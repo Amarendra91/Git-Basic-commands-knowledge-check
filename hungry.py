36d3b9f7a84b27934aee8185109caed371d43d09
amarbulu <amarendra867@gmail.com>
2021-07-27 18:16:59 +0530

    samosa added

--- hungry.py
+++ hungry.py
@@ -2,6 +2,7 @@ hungry = input("Are you hungry?")
 if hungry == "Yes":
     print("eat some food")
     print("eat some junk food")
+    print("eat samosa")
 else:
     print("continue your training.")
 
