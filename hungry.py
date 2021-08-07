073f4535c21cc364b0fd034f88c38933f076d98b
amarbulu <amarendra867@gmail.com>
2021-08-07 20:12:58 +0530

    one more else stmt added

--- hungry.py
+++ hungry.py
@@ -1,10 +1,9 @@
 hungry = input("Are you hungry?")
 if hungry == "Yes":
-    print("eat some food")
-    print("eat some junk food")
     print("eat samosa")
     print("eat chicken")
     print("eat burger")
+
 else:
     print("do your homework")
 
